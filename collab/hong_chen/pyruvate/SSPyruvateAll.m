10d21c3603276d182a2433d242f478a336c40070
brandon <brandon.barker@gmail.com>
2014-01-27 10:09:10 -0500

    new script is working for pyruvate analysis

--- collab/hong_chen/pyruvate/SSPyruvateAll.m
+++ collab/hong_chen/pyruvate/SSPyruvateAll.m
@@ -1,24 +1,42 @@
-function [pdc, biomvec] = SSPyruvateAll(model, itermet, initF, finalF, step)
+function [pdc, biomvec, eth] = SSPyruvateAll(model, itermet, initF, finalF, step)
 %Assumes an irreversible model!
-%O2 uptake: rxn 882
-%glucose  : rxn 794
-%pyruvate : rxn 918
-%PYRDC    : rxn 1958
-pyrRxns = [140 144 238 1762 1770 1958]; %Exclude 1962, a transport rxn.
+%O2 uptake: rxn 
+%glucose  : rxn 
+%pyruvate : rxn 
+%PYRDC    : rxn 
+%ethanol  : rxns   [143 261 262]
+%
+% itermet : usually glucose (rxn = 727), pyruvate (rxn = 797)
 mm = model;
-nIter = (finalF-initF)/step + 1;
+pyrRxns = [238 1609 1805 1806 140 144 240 603 1617]; %Exclude transport rxns.
+                                   %^ These are for iMM904.
+ethRxns = [143 261 262];
+mm.ub(264) = 0; % 264 ends up being a duplicate of 261 
+if itermet == 727
+    mm.ub(797) = 0;
+end
+if itermet == 797
+    mm.ub(727) = 0.1; %need some glucose
+end
+nIter = floor((finalF-initF)/step) + 1;
 mm.lb(itermet) = initF;
-pdc = zeros(length(pyrRxns),nIter);
-biomvec = zeros(1,nIter);
+pdc = zeros(length(pyrRxns), nIter);
+eth = zeros(length(ethRxns) + 1, nIter);
+biomvec = zeros(1, nIter);
 
 for i =1:nIter
   mm.ub(itermet) = 1 * (initF + (i-1)*step);
   disp(mm.ub(itermet));
-  solt = optimizeCbModel(mm,'max',true,true);
+  solt = optimizeCbModel(mm, 'max', 'one');
   pdcratio = solt.x(pyrRxns)/sum(solt.x(pyrRxns));
-  pdc(:,i) = pdcratio';
+  pdc(:, i) = pdcratio';
+  ethsum = sum(solt.x(ethRxns));
+  eth(:, i) = [solt.x(ethRxns)' ethsum]';
   biomvec(i) = solt.f;
 end
+
+save(['PyruvateAll_' mm.rxnNames{itermet} '_' num2str(nIter) '.mat'], ...
+    'pyrRxns', 'pdc', 'biomvec', 'ethRxns', 'eth');
 
 
 
