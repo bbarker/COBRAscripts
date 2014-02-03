63bb08fd9644f32dea085bc6a769d08fb98091b4
brandon <brandon.barker@gmail.com>
2014-02-02 23:45:31 -0500

    minor improvements

--- collab/hong_chen/pyruvate/SSPyruvateAll.m
+++ collab/hong_chen/pyruvate/SSPyruvateAll.m
@@ -8,7 +8,7 @@ function [pdc, biomvec, eth] = SSPyruvateAll(model, itermet, initF, finalF, step
 %
 % itermet : usually glucose (rxn = 727), pyruvate (rxn = 797)
 mm = model;
-pyrRxns = [238 1609 1805 1806 140 144 240 603 1617]; %Exclude transport rxns.
+pyrRxns = [238 1609 1805 140 144 240 1617]; %Exclude transport rxns.
                                    %^ These are for iMM904.
 ethRxns = [143 261 262];
 mm.ub(264) = 0; % 264 ends up being a duplicate of 261 
@@ -28,8 +28,9 @@ for i =1:nIter
   mm.ub(itermet) = 1 * (initF + (i-1)*step);
   disp(mm.ub(itermet));
   solt = optimizeCbModel(mm, 'max', 'one');
-  pdcratio = solt.x(pyrRxns)/sum(solt.x(pyrRxns));
-  pdc(:, i) = pdcratio';
+  %pdcratio = solt.x(pyrRxns)/sum(solt.x(pyrRxns));
+  %pdc(:, i) = pdcratio';
+  pdc(:, i) = solt.x(pyrRxns); %easy to calculate ratio later.
   ethsum = sum(solt.x(ethRxns));
   eth(:, i) = [solt.x(ethRxns)' ethsum]';
   biomvec(i) = solt.f;
