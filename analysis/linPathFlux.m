deae73b0a990d474e3d4c9f584c99139119db8df
brandon <brandon.barker@gmail.com>
2014-02-07 14:42:17 -0500

    initial input for linPathFlux working

--- analysis/linPathFlux.m
+++ analysis/linPathFlux.m
@@ -1,10 +1,17 @@
-function [qVecStart, qVecPrior] linPathFlux(m, varargin)
+function [qVecStart, qVecPrior] = linPathFlux(m, varargin)
+qVecStart = 0;
+qVecPrior = 0;
 % Measures attribution of a (metabolite) flux along a linear
 % pathway in terms of the start of the pathway. 
 %
+% There are two ways to look at this:
+% 1) Percent of X converted to Y along pathway X...Y
+% 2) Percent of Y derived from X along pathway X...Y
+% For now we focus only on 1.
+%
 % metNames are used to define pathways as these are the most
 % general possibility and can even encompass multiple compartments; 
-% for a more specific use, reactions can be used (see isRxn).
+% for a more specific use, reactions can be used..
 
 p = inputParser;
 p.FunctionName = 'linPathFlux';
@@ -17,19 +24,17 @@ end
 %%% Required INPUT %%%
 %
 p.addRequired('m');
-% COBRA model
+% COBRA *irreversible* model
 %
 p.addRequired('linPath', @IPcheck_linPath);
 % A cell array of metNames or alternatively may contain
-% some rxn ids (see isRxn).
+% some rxn ids as a cell array of rxn ids, where
 % 
 %
 %%% Optional INPUT %%%
 %
-p.addParamValue('isRxn', [false], @IPcheck_boolVec);
-% A Boolean vector of the same length as linPath
-% with true entries corresponding to linPath entries
-% that are rxn ids instead of metNames 
+%
+p.addParamValue('rxns', {}, @IPcheck_rxns);
 %
 % OUTPUT
 %
@@ -42,8 +47,43 @@ p.addParamValue('isRxn', [false], @IPcheck_boolVec);
 % can be attributed to the previous flux in the pathway
 
 
-end % of linPathFlux
+p.parse(m, varargin{:});
+arg = p.Results
 
+%Convert linPath into reaction sets.
+linPathRxns = cell(1, length(arg.linPath)-1);
+pathLen = length(arg.linPath);
+for i = 1:(pathLen-1)
+    %Get all substrates
+    substrates = find(strcmp(arg.m.metNames, arg.linPath{i}));
+    products   = find(strcmp(arg.m.metNames, arg.linPath{i+1}));
+    %Get all forward reactions
+    for j = 1:length(substrates)
+        s = substrates(j);
+        for k = 1:length(products)
+            p = products(k);
+            linPathRxns{i} = union(linPathRxns{i}, ...
+                find((arg.m.S(s, :) < 0) & (arg.m.S(p, :) > 0)));
+        end
+    end
+    linPathRxns{i} = setdiff(linPathRxns{i}, [0]);
+end
+
+%Optionally, if reaction sets are explicitly listed for any
+%pathway components, override with them here:
+if length(arg.rxns) > 0
+    for i = 1:(pathLen-1)
+	linPathRxns{i} = zeros(size(arg.rxns{i}));
+	for j = 1:length(linPathRxns{i})
+            if length(arg.rxns{i}(j)) > 0
+	        linPathRxns{i}(j) = ...
+                    find(strcmp(arg.m.metNames, arg.rxns{i}(j)));
+            end
+	end
+    end
+end
+
+end % of linPathFlux
 
 %%%%%%%%%%%%%     Input Parser Functions     %%%%%%%%%%%%%
 
@@ -51,15 +91,19 @@ function TF = IPcheck_linPath(x)
 TF = true;
 %check x is a cell array
 if ~iscell(x)
-    error('linPath must be cell array of strings or cells of strings');
+    error('linPath must be a cell array');
 end
 % check x is 1-d
 sz_x = size(x);
 if min(sz_x) ~= 1 || numel(sz_x) > 2
     error('x is not one dimensional');
 end
-%check each entry is a string or a cell
-%actually, save this as a model-based check later
+%check each entry is a string
+for i = 1:length(x)
+    if ~isstr(x{i})
+        error(['entry ' num2str(i) ' not a string.']); 
+    end
+end
 end % of IPcheck_linPath
 
 function TF = IPcheck_boolVec(x)
