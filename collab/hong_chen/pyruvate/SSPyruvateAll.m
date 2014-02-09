66213fb678bd9460c4ba1e204742e38b6f323ddb
brandon <brandon.barker@gmail.com>
2014-02-08 19:01:08 -0500

    further work towards generalizing to use different models

--- collab/hong_chen/pyruvate/SSPyruvateAll.m
+++ collab/hong_chen/pyruvate/SSPyruvateAll.m
@@ -1,5 +1,5 @@
-function [qMat, fluxMat] = SSPyruvateAll(m, itermet, initF, finalF, step, linPath)
-% Assumes an irreversible model!
+function [qMat, fluxMat] = SSPyruvateAll(mRev, itermet, initF, finalF, step, linPath)
+% Assumes a reversible model! (which is then later converted to irrev)
 
 %
 % This script records the fraction of pyruvate converted to acetaldehyde
@@ -7,38 +7,71 @@ function [qMat, fluxMat] = SSPyruvateAll(m, itermet, initF, finalF, step, linPat
 % levels of glucose (or pyruvate, or some other metabolite) are allowed
 % into the system.
 %
-
+% itermet : usually a carbon source (e.g. 'glucose' or 'pyruvate')
 %
-% Glucose Uptake Rxns
-% iMM904 : 'D Glucose exchange backward'
-% Yeast7 : 'D-glucose exchange backward'
-GlucoseRxns = {'D Glucose exchange backward', 'D-glucose exchange backward'};
 
+minGlucose = 0.1; %Need some glucose to grow in some models.
+
+if ~exist('linPath', 'vars')
+    linPath = {'Pyruvate', 'Acetaldehyde', 'Ethanol'}; %iMM904
+end
+
+% Glucose Uptake Rxns
+% iMM904 : 'D Glucose exchange'
+% Yeast7 : 'D-glucose exchange'
+GlucoseRxns = {'D Glucose exchange', 'D-glucose exchange'};
+GlucoseRxnsBack = rNameBack(GlucoseRxns);
 %
 % Pyruvate Uptake Rxn
-% iMM904 :
-% Yeast7 :
+% iMM904 : 'Pyruvate exchange'
+% Yeast7 : 'pyruvate exchange'
 %
+PyruvateRxns = {'Pyruvate exchange', 'pyruvate exchange'};
+PyruvateRxnsBack = rNameBack(PyruvateRxns);
+
+%
+% Oxygen Uptake Rxn
+% iMM904 : 'O2 exchange'
+% Yeast7 : 'oxygen exchange'
+%
+OxygenRxns = {'O2 exchange', 'oxygen exchange'};
+OxygenRxnsBack = rNameBack(OxygenRxns);
 
 %
 %Need to set major uptake levels to be the same for various models
 %
-% Carbon source (Glucose, Pyruvate), O2;  phosphorous, ammonia ?
+% Carbon source (Pyruvate uptake), O2;  phosphorous, ammonia ?
 %
 %
-m.ub(...) = ...
 
-% itermet : usually glucose (rxn = 727), pyruvate (rxn = 797)
+O2Upt = rxnIdxFromNames(mRev, OxygenRxns); 
+mRev.lb(O2Upt) = -2; % low default found in iMM904
 
+pyrUpt = rxnIdxFromNames(mRev, PyruvateRxns); 
+mRev.lb(pyrUpt) = -1; % set for convertToIrreversible
+
+glucUpt = rxnIdxFromNames(mRev, GlucoseRxns); 
+%no need to set it here
+
+%
+%
+% Now in irreversible land
+%
+%
+m = convertToIrreversible(mRev);
 nRxns = length(m.rxns);
-if itermet == 727
-    m.ub(797) = 0;
-end
-if itermet == 797
-    m.ub(727) = 0.1; %need some glucose
-end
-if ~exist('linPath', 'vars')
-    linPath = {'Pyruvate', 'Acetaldehyde', 'Ethanol'}; %iMM904
+
+O2Upt = rxnIdxFromNames(m, OxygenRxnsBack); 
+pyrUpt = rxnIdxFromNames(m, PyruvateRxnsBack); 
+glucUpt = rxnIdxFromNames(m, GlucoseRxnsBack); 
+
+%Not enough options to worry about using a map for now.
+if strcmp(itermet, 'pyruvate')
+    itermet = pyrUpt;
+    m.ub(glucUpt) = minGlucose; %need some glucose
+elseif strcmp(itermet, 'glucose')
+    itermet = glucUpt;
+    m.ub(pyrUpt) = 0;
 end
 
 nIter = floor((finalF-initF)/step) + 1;
@@ -68,6 +101,19 @@ save(['PyruvateAll_' m.rxnNames{itermet} '_' num2str(nIter) '.mat'], ...
       'qMat', 'fluxMat', 'pathInfo');
 
 
+end % of SSPyruvateAll
 
+function rxn = rxnIdxFromNames(m, rNames)
+% Given a list of rxnNames for the same reaction in various
+% models, return the reaction index for the first found rNames
+% in m.rxnNames.
+[~, rIdx] = ismember(rNames, m.rxnNames);
+rIdxNotZero = find(rIdx);
+rxn = rIdx(rIdxNotZero(1));
+end % of rxnIdxFromNames
 
+function rNamesIrr = rNameBack(rNames)
+rNamesIrr = cellfun(@(x) strcat(x, ' backward'), rNames, ... 
+    'UniformOutput', false);
+end % of rNameBack
 
