306646ad15ede685df661ea04aa67b11fee3860e
Brandon <brandon.barker@gmail.com>
2014-03-12 11:50:26 -0400

    more plotting options for reaction subsets

--- analysis/linPathFlux_plot.m
+++ analysis/linPathFlux_plot.m
@@ -1,4 +1,4 @@
-function linPathFlux_plot(qMat, baseColor, xVec, xLabel, linPath)
+function linPathFlux_plot(qMat, baseColor, xVec, xLabel, pathIdxs, linPath)
 % Given a consecutive list of outputps from a script calling linPathFlux 
 % (in matrix form: qMat), plots them in the order presented as 
 % line plots, with the first entries the darkest shade of the specified
@@ -12,6 +12,9 @@ function linPathFlux_plot(qMat, baseColor, xVec, xLabel, linPath)
 %
 % linPath: The same path passed to linPathFlux.
 %          Used to construct legends.
+%
+% pathIdxs: Only plot these reactions.
+%
 
 white = [1 1 1];
 
@@ -30,26 +33,34 @@ if ~exist('xLabel', 'var')
     xLabel = '';
 end 
 
+if ~exist('pathIdxs', 'var')
+    pathIdxs = 1:pathLenM1;
+end 
+
 figure();
 set(gcf, 'Position', get(0,'Screensize')); % Maximize figure.
-set(gca, 'FontSize', 23);
+set(gca, 'FontSize', 28);
 hold all;
 
 for i = pathLenM1:-1:1
-    iColor = baseColor + ((i-1)/pathLenM1) * (1-maxBC) * white;
-    plot(xVec, qMat(:, i)', 'Color', iColor, 'LineWidth', 3);
+    if any(ismember(pathIdxs, i))
+        iColor = baseColor + ((i-1)/pathLenM1) * (1-maxBC) * white;
+        plot(xVec, qMat(:, i)', 'Color', iColor, 'LineWidth', 3);
+    end
 end
 xlabel(xLabel);
 if exist('linPath', 'var')
     ylabel(['Fraction of ' linPath{1} ' converted.']);
 else
-    ylabel('Fraction of pathway entry metabolite converted.');
+    ylabel('Fraction of entry metabolite converted.');
 end
 
 if exist('linPath', 'var')
     legEntries = cell(1, pathLenM1);
     for i = 1:pathLenM1
-        legEntries{i} = [linPath{i} ' -> ' linPath{i+1}]; 
+        if any(ismember(pathIdxs, i))
+            legEntries{i} = [linPath{i} ' -> ' linPath{i+1}]; 
+        end
     end
     legend(legEntries{:});
 end
