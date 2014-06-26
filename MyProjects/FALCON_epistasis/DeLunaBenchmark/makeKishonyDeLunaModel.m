599702fd2d8db14b473e4292ed701e9847040eec
Brandon Barker <brandon.barker@gmail.com>
2014-06-25 23:12:55 -0400

    started work on Kishony/DeLuna benchmark

---
+++ MyProjects/FALCON_epistasis/DeLunaBenchmark/makeKishonyDeLunaModel.m
@@ -0,0 +1,25 @@
+function modelOut = makeKishonyDeLunaModel(modelIn)
+
+%Growth rate should be similar to standard YPD medium.
+
+modelOut = modelIn;
+
+
+% Some constants
+LBmin = -1000;
+
+% Adjust glucose uptake:
+idx = find(strcmp(modelOut.rxnNames, 'D-glucose exchange'));
+modelOut.lb(idx) = -10;
+
+% Change nitrogen source:
+idx = find(strcmp(modelOut.rxnNames, 'ammonium exchange'));
+modelOut.lb(idx) = 0;
+idx = find(strcmp(modelOut.rxnNames, 'L-proline exchange'));
+modelOut.lb(idx) = -1;
+
+%
+% Verify uptakes
+%
+[selExc, selUpt] = findExcRxns(modelOut);
+modelOut.rxnNames(selUpt)
